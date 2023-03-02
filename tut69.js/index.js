f0d2b4682ef6ffb15bd58ca479a4205cd07cc6e5
Aditya Nandwana <you@example.com>
2023-03-03 02:05:18 +0530

    Initial commit 69

---
+++ tut69.js/index.js
@@ -0,0 +1,7 @@
+console.log("Hi, This is tutorial 690")
+
+const express = require('express')
+const app = express()
+
+// If i only want dependencies on development not in deployment
+// then we can use npm i ${module} --save-dev
