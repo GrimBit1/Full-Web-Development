195fc7076707f6f8b2e92c7afae93812b3bc527f
Aditya Nandwana <you@example.com>
2023-03-03 22:38:40 +0530

    Initial commit72

--- tu70(Express).js/app.js
+++ tu70(Express).js/app.js
@@ -1,1 +1,16 @@
-console.log("Hi")
+console.log("Hi");
+const port = 80;
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("We got a request");
+  res.status(200).send("Hi"); // to also send the status code
+//   res.send("Hi");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running in port ${port}`);
+});
