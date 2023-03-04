00c09226a70c67600306efda211c5d1a43168a77
Aditya Nandwana <you@example.com>
2023-03-05 01:44:57 +0530

    Initial commit 70

--- tu70(Express).js/app.js
+++ tu70(Express).js/app.js
@@ -1,14 +1,18 @@
 console.log("Hi");
+
 const port = 80;
 
 const express = require("express");
 
 const app = express();
 
+app.use("/", express.static("static"));
 app.get("/", (req, res) => {
   console.log("We got a request");
-  res.status(200).send("Hi"); // to also send the status code
-//   res.send("Hi");
+
+  res.status(200).send("Hi");
+  // to also send the status code
+  //   res.send("Hi");
 });
 
 app.listen(port, () => {
